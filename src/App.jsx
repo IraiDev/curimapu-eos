88873239172073c6be870932f90a1dc940068618
IariDev <iarriagada@zionit.cl>
2021-08-04 18:07:37 -0400

    creacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from "@material-tailwind/react/Button";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>hola mundo</h1>
+      <Button color="lightBlue" ripple="light">Button</Button>
+    </div>
+  )
+}
